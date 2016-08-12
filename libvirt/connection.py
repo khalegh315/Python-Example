b80ec3d517724fd3eebb59a2c2b6631227bf48d1
khalegh315 <khalegh_315@yahoo.com>
2016-08-12 18:12:27 +0430

    Create connection.py

---
+++ libvirt/connection.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import sys
+import libvirt
+
+conn = libvirt.open('qemu:///system')
+if conn == None:
+    print('Failed to open connection to qemu:///system', file=sys.stderr)
+    exit(1)
+else:
+    print('Succeed to open connection to qemu:///system')
+exit(0)
