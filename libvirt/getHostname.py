b66e4f3f453f0e9ad811373159d1b26658046494
khalegh315 <khalegh_315@yahoo.com>
2016-08-13 16:32:09 +0430

    Create getHostname.py

---
+++ libvirt/getHostname.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import sys
+import libvirt
+
+conn = libvirt.open('qemu:///system')
+if conn == None:
+    print('Failed to open connection to qemu:///system', file=sys.stderr)
+    exit(1)
+
+host = conn.getHostname()
+print('Hostname:'+host)
+conn.close()
+exit(0)
